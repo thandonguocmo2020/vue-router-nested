bfe5738077471759e977f0a80eaab0ca6e67750d
thandonguocmo2020 <thandonguocmo2020@users.noreply.github.com>
2017-04-25 21:54:01 +0700

    json.js
    menu nested

---
+++ json.js
@@ -0,0 +1,44 @@
+{
+    "424":{
+        "name":"Category 1",
+        "count":4425,
+        "childeren":{
+            "462":{
+                "name":"Category 1.1",
+                "count":1231,
+                "childeren":{
+                    "574":{
+                        "name":"Category 1.1.1",
+                        "count":527
+                    },
+                    "581":{
+                        "name":"Category 1.1.2",
+                        "count":175
+                    },
+                    "583":{
+                        "name":"Category 1.1.3",
+                        "count":153
+                    }
+                }
+            },
+            "432":{
+                "name":"Category 1.2",
+                "count":451,
+                "childeren":{
+                }
+            },
+            "132":{
+                "name":"Category 1.3",
+                "count":41,
+                "childeren":{
+                }
+            }
+        }
+    },
+    "125":{
+        "name":"Category 2",
+        "count":45,
+        "childeren":{
+        }
+    }
+}
